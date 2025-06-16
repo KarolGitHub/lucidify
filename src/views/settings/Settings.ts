d35422ff4c4efa345c7bae12ca55f8615af4899b
Karol Gardyjas <karolgardyjas@gmail.com>
2025-06-16 12:16:27 +0200

    style: client - dark mode for notifications scheduler

--- src/views/settings/Settings.ts
+++ src/views/settings/Settings.ts
@@ -1,10 +1,12 @@
 import CardSettings from "@/components/Cards/CardSettings/CardSettings.vue";
 import CardProfile from "@/components/Cards/CardProfile/CardProfile.vue";
+import CardRealityCheckScheduler from "@/components/Cards/CardRealityCheckScheduler/CardRealityCheckScheduler.vue";
 
 export default {
   name: "Settings",
   components: {
     CardSettings,
     CardProfile,
+    CardRealityCheckScheduler,
   },
 };
